227484750f2032e992956328de28dd38d8d33a1d
Caterpillarflys <1057878422@qq.com>
2020-03-26 23:39:47 +0800

    feature: Vuex的使用举例

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/index';
+import store from './store/index';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+}).$mount('#app');
